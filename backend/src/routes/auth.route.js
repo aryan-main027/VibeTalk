fb22b146fd738c81d7457b2e3f15aada7412996b
Aryan Shashwat <aryan.main27@gmail.com>
2026-03-04 16:29:07 +0530

    init

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -6,4 +6,12 @@ authRouter.get('/signup',(req,res)=>{
   res.send("Happy Holi");
 })
 
+authRouter.get('/login',(req,res)=>{
+  res.send("Happy Holi login");
+})
+
+authRouter.get('/logout',(req,res)=>{
+  res.send("Happy Holi logout");
+})
+
 export default authRouter;
