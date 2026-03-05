d0cb790b57d6c21625703149ddb9a176d785ce28
Aryan Shashwat <aryan.main27@gmail.com>
2026-03-05 17:40:11 +0530

    auth for updateProfile

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,12 +1,16 @@
 import { Router } from "express";
-import { signup , login , logout } from "../controllers/auth.controller.js";
-
+import { signup , login , logout , updateProfile} from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 const authRouter = Router();
  
 authRouter.post('/signup',signup);
 
 authRouter.post('/login',login);
 
-authRouter.post('/logout',logout)
+authRouter.post('/logout',logout);
+
+authRouter.put('/update-profile',protectRoute,updateProfile);
+
+authRouter.get("/check", protectRoute, (req, res) => res.status(200).json(req.user));
 
 export default authRouter;
