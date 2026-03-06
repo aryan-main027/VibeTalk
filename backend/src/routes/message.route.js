dc23187da8a0f0b368dd75449c7a575a973ff38d
Aryan Shashwat <aryan.main27@gmail.com>
2026-03-06 16:08:29 +0530

    messages-route

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
-
+import { getAllContacts , getMessageByUserId ,sendMessage , getChatPartners } from "../controllers/message.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 const messageRouter = Router();
 
-messageRouter.get("/send",(req,res)=>{
-  res.send("Messaging ...");
-})
+messageRouter.use(arcjetProtection,protectRoute)
+messageRouter.get("/contacts",getAllContacts);
+messageRouter.get("/chats",getChatPartners);
+messageRouter.get("/:id",getMessageByUserId);
+messageRouter.post("/send/:id",sendMessage);
 
 export default messageRouter;
