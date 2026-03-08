1aca84cf8ece3c0b8f9e595ca8ea6f12faca829d
Aryan Shashwat <aryan.main27@gmail.com>
2026-03-08 16:05:15 +0530

    Login

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { useAuthStore } from "../store/useAuthStore.js";
+import { LogOut } from 'lucide-react';
+
 
 function ChatPage() {
+
+  const { logout } = useAuthStore();
   return (
-    <div>ChatPage</div>
+    <div className='z-10'>
+      Chat Page 
+      <button onClick={logout}>Logout</button>
+    </div>
   )
 }
 
